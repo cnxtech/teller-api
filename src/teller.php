4ade70456f44743ebdf998e6808c160f57371d39
Phil E. Taylor <phil@phil-taylor.com>
2017-06-11 14:55:09 +0100

    Composer init

---
+++ src/teller.php
@@ -0,0 +1,22 @@
+<?php
+namespace Teller;
+
+/**
+ * Sends and iterator of requests concurrently using a capped pool size.
+ *
+ * The pool will read from an iterator until it is cancelled or until the
+ * iterator is consumed. When a request is yielded, the request is sent after
+ * applying the "request_options" request options (if provided in the ctor).
+ *
+ * When a function is yielded by the iterator, the function is provided the
+ * "request_options" array that should be merged on top of any existing
+ * options, and the function MUST then return a wait-able promise.
+ */
+class Teller
+{
+
+	function setApiKey($key)
+	{
+		die('a');
+	}
+}
